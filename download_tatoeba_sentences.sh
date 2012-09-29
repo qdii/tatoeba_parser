f144a81e061d4f31192ddf12826f796f1ced7c84
qdii <victor.lavaud@gmail.com>
2012-09-29 14:57:28 -0400

    Added script to retrieve the data files from tatoeba

---
+++ download_tatoeba_sentences.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sentence_url='http://tatoeba.org/files/downloads/sentences.csv'
+links_url='http://tatoeba.org/files/downloads/links.csv'
+
+echo 'Starting retrieving data from tatoeba.org'
+wget $sentence_url
+wget $links_url
