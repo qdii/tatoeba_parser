c5e3fe5d2e8798e82f3ef09f9fad52b809b4b8e3
qdii <victor.lavaud@gmail.com>
2012-10-01 13:31:55 -0400

    Added tags.csv to the list of file that download_tatoeba_sentences.sh script downloads

--- download_tatoeba_sentences.sh
+++ download_tatoeba_sentences.sh
@@ -2,7 +2,9 @@
 
 sentence_url='http://tatoeba.org/files/downloads/sentences.csv'
 links_url='http://tatoeba.org/files/downloads/links.csv'
+tags_url='http://tatoeba.org/files/downloads/tags.csv'
 
 echo 'Starting retrieving data from tatoeba.org'
 wget $sentence_url
 wget $links_url
+wget $tags_url
