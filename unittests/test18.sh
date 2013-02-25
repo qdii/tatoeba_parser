17b03d53dc84a7228a046d6715cd64f6b7e6ab99
Victor Lavaud (qdii) <victor.lavaud@gmail.com>
2013-02-25 20:48:02 +0100

    Fixed unittest 18

--- unittests/test18.sh
+++ unittests/test18.sh
@@ -3,8 +3,15 @@
 
 mv lists.csv lists.csv.back
 touch lists.csv
-result=`$tatoparser_bin --in-list abc`
-expected_result=0
+result=`$tatoparser_bin --in-list abc &>/dev/null`
+if [ $? -eq 0 ]
+then
+	result=0
+	expected_result=0
+else
+	result=0
+	expected_result=1
+fi
 rm lists.csv
 mv lists.csv.back lists.csv
 
